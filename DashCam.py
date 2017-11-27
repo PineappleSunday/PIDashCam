fa28d167ba436fed7144cfa86cb883fab12f8908
ReitzTennis <colin.runnion@gmail.com>
2017-11-27 07:49:14 -0600

    Add files via upload

--- DashCam.py
+++ DashCam.py
@@ -9,23 +9,22 @@ def Init():
 	camera.resolution =(640,480)
 	return camera,LED
 def Record(camera,LED):
-	date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-	t_end = time.time() + 10 * 1
-	camera.start_recording('Video' +date + '.h264')
+	date = datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+	t_end = time.time() * 60 * 10
+	camera.start_recording(date + '.h264')
 
 	while (time.time() < t_end):
 		print('Recording')
 	camera.stop_recording()
 
 def main():
+    camera,LED = Init()
 
+    while (True):
 
-
-	camera,LED = Init()
-	while True:
-		if (True):
-			print('I am recording')
-			time.sleep(1)
-			Record(camera,LED)
+        if (True):
+            print('I am recording')
+	    time.sleep(1)
+	    Record(camera,LED)
 if __name__ == "__main__":
 	main()
